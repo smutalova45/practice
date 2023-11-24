4d86b4b1f573385b8f74a4779c6456477da24bb4
golang_user <smutalova45@gmail.com>
2023-11-24 16:07:35 +0600

    added

--- hmw2/main.go
+++ hmw2/main.go
@@ -1,9 +1,8 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-
-// )
+import (
+	"fmt"
+)
 
 // func kordinata(x1,x2,y1,y2 float64, c float64){
 
@@ -72,3 +71,301 @@
 // sum = max+min
 // fmt.Printf("max %f , min %f, sum %f ", max, min, sum)
 //}
+
+// homework
+func xonalison(n int) string {
+	var s string
+	if n < 10 && n > 0 {
+		s = "1 xonali"
+		fmt.Println(s)
+	} else if n >= 10 && n < 100 {
+		s = "2 xonali"
+		fmt.Println(s)
+	} else if n >= 100 && n < 1000 {
+		s = "3 xonali"
+		fmt.Println(s)
+	} else if n >= 1000 && n < 10000 {
+		s = "4 xonali"
+		fmt.Println(s)
+	}
+	return s
+}
+func add(a, b int) int {
+	digit := 1
+	for i := b; i > 0; i /= 10 {
+		digit *= 10
+	}
+	result := a*digit + b
+	return result
+}
+// func factorial(n int) int {
+// 	if n == 0 {
+// 		return 1
+// 	}
+// 	return n * factorial(n-1)
+// }
+
+// func reverse(n int) int {
+// 	result := 0
+// 	var k int
+// 	for n > 0 {
+// 		k = n % 10
+// 		result *= 10.
+// 		result += k
+
+// 		n /= 10
+// 	}
+// 	return result
+// }
+
+func son(n int) string {
+ var s string
+	if n > 0 && n < 10 {
+		var k int
+		k = (n / 1) % 10
+		switch k {
+
+		case 1:
+			s = "bir "
+			fmt.Print(s)
+		case 2:
+			s = "ikki "
+			fmt.Print(s)
+		case 3:
+			s = "uch "
+			fmt.Print(s)
+			break
+		case 4:
+			s = "to'rt  "
+			fmt.Print(s)
+		case 5:
+			s = "besh "
+			fmt.Print(s)
+		case 6:
+			s = "olti "
+			fmt.Print(s)
+		case 7:
+			s = "yetti "
+			fmt.Print(s)
+		case 8:
+			s = "sakkiz "
+			fmt.Print(s)
+		case 9:
+			s = "to'qqiz "
+			fmt.Print(s)
+		}
+	}else if n>9 && n<100{
+		var k int
+	
+		k = (n / 100) % 10
+		switch k {
+		case 1:
+			s = "on "
+			fmt.Print(s)
+		case 2:
+			s = "yigirma"
+			fmt.Print(s)
+			
+		case 3:
+			s = "ottiz "
+			fmt.Print(s)
+		case 4:
+			s = "qirq "
+			fmt.Print(s)
+		case 5:
+			s = "ellik"
+			fmt.Print(s)
+			
+		case 6:
+			s = "oltmush "
+			fmt.Print(s)
+		case 7:
+			s = "yettimush"
+			fmt.Print(s)
+		case 8:
+			s = "sakson "
+			fmt.Print(s)
+		case 9:
+			s = "to'qson"
+			fmt.Print(s)
+
+		k = (n / 1) % 10
+		switch k {
+
+		case 1:
+			s = "bir "
+			fmt.Print(s)
+		case 2:
+			s = "ikki "
+			fmt.Print(s)
+		case 3:
+			s = "uch "
+			fmt.Print(s)
+			break
+		case 4:
+			s = "to'rt  "
+			fmt.Print(s)
+		case 5:
+			s = "besh "
+			fmt.Print(s)
+		case 6:
+			s = "olti "
+			fmt.Print(s)
+		case 7:
+			s = "yetti "
+			fmt.Print(s)
+		case 8:
+			s = "sakkiz "
+			fmt.Print(s)
+		case 9:
+			s = "to'qqiz "
+			fmt.Print(s)
+		}
+		}
+    } else if n>99 && n < 1000 {
+		var k int
+		k = (n / 100) % 10
+		switch k {
+		case 1:
+			s = "bir yuz "
+			fmt.Print(s)
+		case 2:
+			s = "ikki yuz "
+			fmt.Print(s)
+			
+		case 3:
+			s = "uch yuz "
+			fmt.Print(s)
+		case 4:
+			s = "to'rt yuz"
+			fmt.Print(s)
+		case 5:
+			s = "besh yuz "
+			fmt.Print(s)
+			
+		case 6:
+			s = "olti yuz "
+			fmt.Print(s)
+		case 7:
+			s = "yetti yuz "
+			fmt.Print(s)
+		case 8:
+			s = "sakkiz yuz "
+			fmt.Print(s)
+		case 9:
+			s = "to'qqiz yuz"
+			fmt.Print(s)
+		}
+
+		k = (n % 100) / 10 //490
+		switch k {
+		case 1:
+			s = "on "
+			fmt.Print(s)
+		case 2:
+			s = "yigirma"
+			fmt.Print(s)
+			
+		case 3:
+			s = "ottiz "
+			fmt.Print(s)
+		case 4:
+			s = "qirq "
+			fmt.Print(s)
+		case 5:
+			s = "ellik"
+			fmt.Print(s)
+			
+		case 6:
+			s = "oltmush "
+			fmt.Print(s)
+		case 7:
+			s = "yettimush"
+			fmt.Print(s)
+		case 8:
+			s = "sakson "
+			fmt.Print(s)
+		case 9:
+			s = "to'qson"
+			fmt.Print(s)
+
+
+		} 
+		k = (n / 1) % 10
+		switch k {
+
+		case 1:
+			s = "bir "
+			fmt.Print(s)
+		case 2:
+			s = "ikki "
+			fmt.Print(s)
+		case 3:
+			s = "uch "
+			fmt.Print(s)
+			break
+		case 4:
+			s = "to'rt  "
+			fmt.Print(s)
+		case 5:
+			s = "besh "
+			fmt.Print(s)
+		case 6:
+			s = "olti "
+			fmt.Print(s)
+		case 7:
+			s = "yetti "
+			fmt.Print(s)
+		case 8:
+			s = "sakkiz "
+			fmt.Print(s)
+		case 9:
+			s = "to'qqiz "
+			fmt.Print(s)
+		}
+
+
+
+		
+
+	}
+
+	
+
+
+return s
+}
+
+func main() {
+
+	//1
+	// var n int
+	// fmt.Scan(&n)
+	// xonalison(n)
+
+	//2
+	// var (
+	// 	a ,b int
+	// )
+	//   fmt.Scan(&a)
+	//   fmt.Scan(&b)
+	//   fmt.Print(add(a,b))
+
+	//3
+	// var n int
+	// fmt.Scan(&n)
+	// factorial(n)
+
+	//4 reverse
+
+	// var n int
+	// fmt.Scan(&n)
+	// fmt.Print(reverse(n))
+
+
+	//4 
+	var  n int 
+	fmt.Scan(&n)
+	son(n)
+
+}
